5f8a6f0cee4163461f0615ca7a690a116d45ea36
YuqianZhang <yuqian0320@gmail.com>
2017-10-12 18:08:37 -0400

    Update server.py

--- server.py
+++ server.py
@@ -13,70 +13,74 @@
 # limitations under the License.
 
 """
-Customer
+Customer Service
 
-This is an example of a shop service written with Python Flask
-It demonstraits how a RESTful service should be implemented.
-
-Paths
------
-GET  /customers - Retrieves a list of customers from the database
-GET  /customers{id} - Retrirves a Customer with a specific id
-POST /customers - Creates a Customer in the datbase from the posted database
-PUT  /customers/{id} - Updates a Customer in the database fom the posted database
-DELETE /customers{id} - Removes a Customer from the database that matches the id
+Paths:
+------
+GET /customers - Returns a list all of the customers
+GET /customers/{id} - Returns the Customer with a given id number
+POST /customers - creates a new Customer record in the database
+PUT /customers/{id} - updates a Customer record in the database
+DELETE /customers/{id} - deletes a Customer record in the database
 """
 
 import os
+import sys
 import logging
-from flask import Flask, Response, jsonify, request, json, url_for, make_response
+from flask import Flask, jsonify, request, url_for, make_response
+from flask_api import status    # HTTP Status Codes
+from werkzeug.exceptions import NotFound
 from models import Customer, DataValidationError
-
-# Pull options from environment
-DEBUG = (os.getenv('DEBUG', 'False') == 'True')
-PORT = os.getenv('PORT', '5000')
-HOST = os.getenv('HOST','0.0.0.0')
 
 # Create Flask application
 app = Flask(__name__)
 
-# Status Codes
-HTTP_200_OK = 200
-HTTP_201_CREATED = 201
-HTTP_204_NO_CONTENT = 204
-HTTP_400_BAD_REQUEST = 400
-HTTP_404_NOT_FOUND = 404
-HTTP_409_CONFLICT = 409
+# Pull options from environment
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+PORT = os.getenv('PORT', '5000')
 
 ######################################################################
 # Error Handlers
 ######################################################################
 @app.errorhandler(DataValidationError)
 def request_validation_error(error):
-    """ Handles all data validation issues from the model """
+    """ Handles Value Errors from bad data """
     return bad_request(error)
 
 @app.errorhandler(400)
 def bad_request(error):
-    """ Handles requests that have bad or malformed data """
-    return jsonify(status=400, error='Bad Request', message=error.message), 400
+    """ Handles bad reuests with 400_BAD_REQUEST """
+    message = error.message or str(error)
+    app.logger.info(message)
+    return jsonify(status=400, error='Bad Request', message=message), 400
 
 @app.errorhandler(404)
 def not_found(error):
-    """ Handles Customers that cannot be found """
-    return jsonify(status=404, error='Not Found', message=error.message), 404
+    """ Handles resources not found with 404_NOT_FOUND """
+    message = error.message or str(error)
+    app.logger.info(message)
+    return jsonify(status=404, error='Not Found', message=message), 404
 
 @app.errorhandler(405)
 def method_not_supported(error):
-    """ Handles bad method calls """
-    return jsonify(status=405, error='Method not Allowed',
-                   message='Your request method is not supported.' \
-                   ' Check your HTTP method and try again.'), 405
+    """ Handles unsuppoted HTTP methods with 405_METHOD_NOT_SUPPORTED """
+    message = error.message or str(error)
+    app.logger.info(message)
+    return jsonify(status=405, error='Method not Allowed', message=message), 405
+
+@app.errorhandler(415)
+def mediatype_not_supported(error):
+    """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
+    message = error.message or str(error)
+    app.logger.info(message)
+    return jsonify(status=415, error='Unsupported media type', message=message), 415
 
 @app.errorhandler(500)
 def internal_server_error(error):
-    """ Handles catostrophic errors """
-    return jsonify(status=500, error='Internal Server Error', message=error.message), 500
+    """ Handles unexpected server error with 500_SERVER_ERROR """
+    message = error.message or str(error)
+    app.logger.info(message)
+    return jsonify(status=500, error='Internal Server Error', message=message), 500
 
 
 ######################################################################
@@ -84,90 +88,131 @@ def internal_server_error(error):
 ######################################################################
 @app.route('/')
 def index():
-    """ Return something useful by default """
+    """ Root URL response """
     return jsonify(name='Customer Demo REST API Service',
                    version='1.0',
-                   url=url_for('list_customers', _external=True)), HTTP_200_OK
+                   paths=url_for('list_customers', _external=True)
+                  ), status.HTTP_200_OK
 
 ######################################################################
-# LIST ALL CustomerS
+# LIST ALL CUSTOMERS
 ######################################################################
 @app.route('/customers', methods=['GET'])
 def list_customers():
-    """ Retrieves a list of customers from the database """
-    results = []
-    lastname = request.args.get('lastname')
-    if lastname:
-        results = Customer.find_by_lastname(lastname)
+    """ Returns all of the Customers """
+    customers = []
+    category = request.args.get('category')
+    name = request.args.get('name')
+    if category:
+        customers = Customer.find_by_lastname(lastname)
+    elif name:
+        customers = Customer.find_by_firstname(firstname)
     else:
-        results = Customer.all()
+        customers = Customer.all()
 
-    return jsonify([customer.serialize() for customer in results]), HTTP_200_OK
+    results = [customer.serialize() for customer in customers]
+    return make_response(jsonify(results), status.HTTP_200_OK)
 
-######################################################################
-# RETRIEVE A Customer
-######################################################################
-@app.route('/customers/<int:id>', methods=['GET'])
-def get_customers(id):
-    """ Retrieves a Customer with a specific id """
-    customer = Customer.find(id)
-    if customer:
-        message = customer.serialize()
-        return_code = HTTP_200_OK
-    else:
-        message = {'error' : 'Customer with id: %s was not found' % str(id)}
-        return_code = HTTP_404_NOT_FOUND
-
-    return jsonify(message), return_code
 
 ######################################################################
-# ADD A NEW Customer
+# RETRIEVE A CUSTOMER
+######################################################################
+@app.route('/customers/<int:customer_id>', methods=['GET'])
+def get_customers(customer_id):
+    """
+    Retrieve a single Customer
+
+    This endpoint will return a Customer based on it's id
+    """
+    customer = Customer.find(customer_id)
+    if not customer:
+        raise NotFound("Customer with id '{}' was not found.".format(customer_id))
+    return make_response(jsonify(customer.serialize()), status.HTTP_200_OK)
+
+
+######################################################################
+# ADD A NEW CUSTOMER
 ######################################################################
 @app.route('/customers', methods=['POST'])
 def create_customers():
-    """ Creates a Customer in the datbase from the posted database """
-    payload = request.get_json()
+    """
+    Creates a Customer
+    This endpoint will create a Customer based the data in the body that is posted
+    """
     customer = Customer()
-    customer.deserialize(payload)
+    customer.deserialize(request.get_json())
     customer.save()
     message = customer.serialize()
-    response = make_response(jsonify(message), HTTP_201_CREATED)
-    response.headers['Location'] = url_for('get_customers', id=customer.id, _external=True)
-    return response
+    location_url = url_for('get_customers', customer_id=customer.id, _external=True)
+    return make_response(jsonify(message), status.HTTP_201_CREATED,
+                         {
+                             'Location': location_url
+                         })
+
 
 ######################################################################
-# UPDATE AN EXISTING Customer
+# UPDATE AN EXISTING CUSTOMER
 ######################################################################
-@app.route('/customers/<int:id>', methods=['PUT'])
-def update_customers(id):
-    """ Updates a Customer in the database fom the posted database """
-    customer = Customer.find(id)
-    if customer:
-        payload = request.get_json()
-        customer.deserialize(payload)
-        customer.save()
-        message = customer.serialize()
-        return_code = HTTP_200_OK
-    else:
-        message = {'error' : 'Customer with id: %s was not found' % str(id)}
-        return_code = HTTP_404_NOT_FOUND
+@app.route('/customers/<int:customer_id>', methods=['PUT'])
+def update_customers(customer_id):
+    """
+    Update a Customer
 
-    return jsonify(message), return_code
+    This endpoint will update a Customer based the body that is posted
+    """
+    customer = Customer.find(customer_id)
+    if not customer:
+        raise NotFound("Customer with id '{}' was not found.".format(customer_id))
+    customer.deserialize(request.get_json())
+    customer.id = customer_id
+    customer.save()
+    return make_response(jsonify(customer.serialize()), status.HTTP_200_OK)
+
 
 ######################################################################
-# DELETE A Customer
+# DELETE A CUSTOMER
 ######################################################################
-@app.route('/customers/<int:id>', methods=['DELETE'])
-def delete_customers(id):
-    """ Removes a Customer from the database that matches the id """
-    customer = Customer.find(id)
+@app.route('/customers/<int:customer_id>', methods=['DELETE'])
+def delete_customers(customer_id):
+    """
+    Delete a Customer
+
+    This endpoint will delete a Customer based the id specified in the path
+    """
+    customer = Customer.find(customer_id)
     if customer:
         customer.delete()
-    return make_response('', HTTP_204_NO_CONTENT)
+    return make_response('', status.HTTP_204_NO_CONTENT)
+
+######################################################################
+#  U T I L I T Y   F U N C T I O N S
+######################################################################
+
+def initialize_logging(log_level):
+    """ Initialized the default logging to STDOUT """
+    if not app.debug:
+        print 'Setting up logging...'
+        # Set up default logging for submodules to use STDOUT
+        # datefmt='%m/%d/%Y %I:%M:%S %p'
+        fmt = '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+        logging.basicConfig(stream=sys.stdout, level=log_level, format=fmt)
+        # Make a new log handler that uses STDOUT
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(logging.Formatter(fmt))
+        handler.setLevel(log_level)
+        # Remove the Flask default handlers and use our own
+        handler_list = list(app.logger.handlers)
+        for log_handler in handler_list:
+            app.logger.removeHandler(log_handler)
+        app.logger.addHandler(handler)
+        app.logger.setLevel(log_level)
+        app.logger.info('Logging handler established')
+
 
 ######################################################################
 #   M A I N
 ######################################################################
 if __name__ == "__main__":
-    # dummy data for testing
+    print "Customer Service Starting..."
+    initialize_logging(logging.INFO)
     app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
