d1ca6b3f9d7dbdc0be505d4318cd06c69012784a
Bq18 <32425146+Bq18@users.noreply.github.com>
2017-10-14 10:26:24 -0400

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -21,13 +21,13 @@ class TestCustomerServer(unittest.TestCase):
     def setUpClass(cls):
         """ Run once before all tests """
         server.app.debug = False
-#        server.initialize_logging(logging.ERROR)
+        server.initialize_logging(logging.ERROR)
 
     def setUp(self):
         """ Runs before each test """
         server.Customer.remove_all()
-        server.Customer(0, 'William', 'Smith').save()
-        server.Customer(0, 'Scott', 'Sun').save()
+        server.Customer(0, 'fido', 'dog').save()
+        server.Customer(0, 'kitty', 'cat').save()
         self.app = server.app.test_client()
 
     def tearDown(self):
@@ -39,7 +39,7 @@ class TestCustomerServer(unittest.TestCase):
         resp = self.app.get('/')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = json.loads(resp.data)
-        self.assertEqual(data['firstname'], 'Customer Demo REST API Service')
+        self.assertEqual(data['name'], 'Customer Demo REST API Service')
 
     def test_get_customer_list(self):
         """ Get a list of Customers """
@@ -49,11 +49,11 @@ class TestCustomerServer(unittest.TestCase):
         self.assertEqual(len(data), 2)
 
     def test_get_customer(self):
-        """ Get one customer """
-        resp = self.app.get('/customer/2')
+        """ Get one Customer """
+        resp = self.app.get('/customers/2')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = json.loads(resp.data)
-        self.assertEqual(data['fistname'], 'Scott')
+        self.assertEqual(data['firstname'], 'kitty')
 
     def test_get_customer_not_found(self):
         """ Get a Customer thats not found """
@@ -61,11 +61,11 @@ class TestCustomerServer(unittest.TestCase):
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_create_customer(self):
-        """ Create a customer """
+        """ Create a Customer """
         # save the current number of customers for later comparrison
         customer_count = self.get_customer_count()
         # add a new customer
-        new_customer = {'firstname': 'Yuxi', 'lastname': 'Zhang'}
+        new_customer = {'firstname': 'sammy', 'lastname': 'snake'}
         data = json.dumps(new_customer)
         resp = self.app.post('/customers', data=data, content_type='application/json')
         self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
@@ -74,7 +74,7 @@ class TestCustomerServer(unittest.TestCase):
         self.assertIsNotNone(location)
         # Check the data is correct
         new_json = json.loads(resp.data)
-        self.assertEqual(new_json['firstname'], 'Yuxi')
+        self.assertEqual(new_json['firstname'], 'sammy')
         # check that count has gone up and includes sammy
         resp = self.app.get('/customers')
         data = json.loads(resp.data)
@@ -83,27 +83,27 @@ class TestCustomerServer(unittest.TestCase):
         self.assertIn(new_json, data)
 
     def test_update_customer(self):
-        """ Update a customer """
-        new_customer_scott = {'fistname': 'scott', 'lastname': 'sun'}
-        data = json.dumps(new_customer_scott)
+        """ Update a Customer """
+        new_kitty = {'firstname': 'kitty', 'lastname': 'tabby'}
+        data = json.dumps(new_kitty)
         resp = self.app.put('/customers/2', data=data, content_type='application/json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         resp = self.app.get('/customers/2', content_type='application/json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         new_json = json.loads(resp.data)
-        self.assertEqual(new_json['lastname'], 'sun')
+        self.assertEqual(new_json['lastname'], 'tabby')
 
-    def test_update_customer_with_no_name(self):
-        """ Update a Customer with no name """
-        new_customer = {'lastname': 'chen'}
+    def test_update_customer_with_no_firstname(self):
+        """ Update a Customer with no firstname """
+        new_customer = {'lastname': 'dog'}
         data = json.dumps(new_customer)
         resp = self.app.put('/customers/2', data=data, content_type='application/json')
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_update_customer_not_found(self):
         """ Update a Customer that can't be found """
-        new_customer = {"lastname": "huri", "firstname": "horse"}
-        data = json.dumps(new_customer)
+        new_kitty = {"firstname": "timothy", "lastname": "mouse"}
+        data = json.dumps(new_kitty)
         resp = self.app.put('/customers/0', data=data, content_type='application/json')
         self.assertEquals(resp.status_code, status.HTTP_404_NOT_FOUND)
 
@@ -120,7 +120,7 @@ class TestCustomerServer(unittest.TestCase):
 
     def test_create_customer_with_no_firstname(self):
         """ Create a Customer with the name missing """
-        new_customer = {'lastname': 'Dog'}
+        new_customer = {'lastname': 'dog'}
         data = json.dumps(new_customer)
         resp = self.app.post('/customers', data=data, content_type='application/json')
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
@@ -131,44 +131,44 @@ class TestCustomerServer(unittest.TestCase):
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_query_customer_list_by_lastname(self):
-        """ Query Customers by lastname """
-        resp = self.app.get('/customers', query_string='lastname = Sun')
+        """ Query Customers by Lastname """
+        resp = self.app.get('/customers', query_string='lastname=dog')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertTrue(len(resp.data) > 0)
-        self.assertTrue('Scott' in resp.data)
-        self.assertFalse('kitty' in resp.data)
+        self.assertTrue('fido' in resp.data)
+        self.assertFalse('Dada' in resp.data)
         data = json.loads(resp.data)
         query_item = data[0]
-        self.assertEqual(query_item['lastname'], 'Sun')
+        self.assertEqual(query_item['lastname'], 'dog')
 
     def test_query_customer_list_by_firstname(self):
         """ Query Customers by Name """
-        resp = self.app.get('/customers', query_string='firstname=Yuxi')
+        resp = self.app.get('/customers', query_string='firstname=fido')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertTrue(len(resp.data) > 0)
-        self.assertTrue('Yuxi' in resp.data)
-        self.assertFalse('kitty' in resp.data)
+        self.assertTrue('fido' in resp.data)
+        self.assertFalse('Miamia' in resp.data)
         data = json.loads(resp.data)
         query_item = data[0]
-        self.assertEqual(query_item['firstname'], 'Yuxi')
+        self.assertEqual(query_item['firstname'], 'fido')
 
-    # def test_method_not_allowed(self):
-    #     """ Call a Method thats not Allowed """
-    #     resp = self.app.post('/customers/0')
-    #     self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    def test_method_not_allowed(self):
+         """ Call a Method thats not Allowed """
+         resp = self.app.post('/customers/0')
+         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    # @patch('server.customer.find_by_name')
+    # @patch('server.Customer.find_by_firstname')
     # def test_bad_request(self, bad_request_mock):
-    #     """ Test a Bad Request error from Find By Name """
+    #     """ Test a Bad Request error from Find By firstname """
     #     bad_request_mock.side_effect = ValueError()
-    #     resp = self.app.get('/customers', query_string='name=fido')
+    #     resp = self.app.get('/customers', query_string='firstname=fido')
     #     self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # @patch('server.customer.find_by_name')
+    # @patch('server.Customer.find_by_firstname')
     # def test_mock_search_data(self, customer_find_mock):
     #     """ Test showing how to mock data """
-    #     customer_find_mock.return_value = [MagicMock(serialize=lambda: {'name': 'fido'})]
-    #     resp = self.app.get('/customers', query_string='name=fido')
+    #     customer_find_mock.return_value = [MagicMock(serialize=lambda: {'firstname': 'fido'})]
+    #     resp = self.app.get('/customers', query_string='firstname=fido')
     #     self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
 
